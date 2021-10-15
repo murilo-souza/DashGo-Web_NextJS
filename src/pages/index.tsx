cf2a48768a39761f95218753905e4db03dc11556
murilo-souza <murilo.l.souza232@gmail.com>
2021-10-14 21:51:55 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,49 @@
+import {Flex, Input, Button, Stack, FormLabel, FormControl} from '@chakra-ui/react'
+
 
 export default function Home() {
   return (
-    <h1>DashGo</h1>
+    <Flex w="90vw" h="100vh" align="center" justify="center">
+      <Flex as="form" width="100%" maxWidth={360} bg="gray.800" p="8" borderRadius={8} flexDir="column" justify="center">
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email" 
+              type="email" 
+              name="email" 
+              focusBorderColor="pink.500"
+              bgColor="gray.900" 
+              variant="filled"
+              width={328}
+              _hover={{
+                bgColor: 'gray.900',
+              }}
+              size="lg"
+              
+              />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input 
+              type="password" 
+              name="password" 
+              id="password" 
+              focusBorderColor="pink.500" 
+              bgColor="gray.900" 
+              variant="filled"
+              width={328}
+              _hover={{
+                bgColor: 'gray.900',
+              }}
+              size="lg"
+              />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
