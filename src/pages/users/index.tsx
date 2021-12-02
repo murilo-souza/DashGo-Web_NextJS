3aaf45396e5e461bca515f398dcf926347e9c5e7
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2021-12-02 19:34:34 -0300

    update

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,12 +5,12 @@ import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 import Link from 'next/link'
 import {useQuery} from 'react-query'
+import { api } from '../../services/api';
 
 export default function UserList(){
-    const {data, isLoading, error} = useQuery('users', async ()=>{
+    const {data, isLoading, error, isFetching, refetch} = useQuery('users', async ()=>{
        
-        const response = await fetch('http://localhost:3000/api/users')
-        const data = await response.json()
+        const {data} = await api.get('users')
 
         const users =  data.users.map(user=>{
             return {
@@ -42,7 +42,10 @@ export default function UserList(){
                 <Sidebar/>
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/>}
+                        </Heading>
                         <Link href="/users/create" passHref>
                             <Button
                                 as="a" 
